5059e49323feec8a3a86b9c612dde91801a6cdb9
FthRobby <rbysalam10@gmail.com>
2022-08-26 21:58:13 +0700

    fixing wabhashhistory error

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
-// import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import HomePage from "../views/HomePage.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
