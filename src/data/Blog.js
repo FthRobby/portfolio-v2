c9347077efbfe435f5fa38e7d4507c1a3396be00
FthRobby <rbysalam10@gmail.com>
2022-09-05 19:06:57 +0700

    add first blog data

---
+++ src/data/Blog.js
@@ -0,0 +1,28 @@
+export const blog = {
+  blog: [
+    {
+      id: 1,
+      title: "First blog push test",
+      description: "This is the description of the first blog push test",
+      date: "Agustus 05, 2022",
+      author: "robi ganteng",
+      gambar:
+        "https://images.unsplash.com/photo-1648737966614-55e58b5e3caf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
+      heading: [
+        "Cupcake ipsum dolor.",
+        "Cake lollipop muffin cake lemon drops.",
+      ],
+      paragraf: [
+        "Sit amet pastry cake toffee carrot cake. Cheesecake candy I love dragée cake jelly-o pie. Cheesecake sesame snaps danish lemon drops sesame snaps sugar plum cupcake powder. Cookie sweet wafer. Jelly chocolate cake dragée candy canes halvah.",
+
+        "Soufflé marzipan pastry topping cheesecake. Jelly marshmallow jelly-o lollipop cake. Candy dragée sweet roll dessert marshmallow. Biscuit apple pie sweet roll candy canes sweet roll sweet chupa chups pudding marshmallow. Brownie croissant biscuit. Ice cream pastry powder wafer carrot cake cotton candy jujubes jelly beans jelly.",
+
+        "Pudding chupa chups pudding gummies sweet sugar plum candy canes gummi bears. Dessert cake soufflé cupcake gingerbread. Cake lemon drops oat cake danish jelly-o icing. Caramels ice cream pudding jujubes donut. Gummies pudding cookie bear claw jujubes lollipop fruitcake muffin. Chupa chups lemon drops cupcake candy I love. Muffin I love oat cake icing candy canes ice cream. Lemon drops marshmallow liquorice wafer caramels.",
+
+        "I love cake soufflé sweet roll sugar plum powder. Tiramisu I love carrot cake. Sugar plum sesame snaps sesame snaps cheesecake. Powder cookie carrot cake tiramisu liquorice carrot cake marzipan gummi bears donut. Lemon drops tart I love toffee I love liquorice sweet roll marzipan I love. Donut bonbon soufflé soufflé chocolate cake jelly. Candy canes ice cream sugar plum. Pie cake gingerbread lemon drops.",
+
+        "Halvah I love marshmallow. Gummi bears tiramisu candy I love brownie I love. Chocolate cake wafer croissant. Halvah icing candy canes tart cake I love soufflé chupa chups. Tootsie roll candy canes candy canes dragée. Oat cake pastry topping jelly beans I love tootsie roll. I love candy jelly donut dessert dessert dessert pie marzipan.",
+      ],
+    },
+  ],
+};
---
+++ src/views/BlogDetail.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="w-full px-6 antialiased mb-10">
+
+    <header class="pt-16 pb-9 sm:pb-16 text-center lg:mt-10 mt-5">
+      <div class="text-sm dark:text-white text-neutral breadcrumbs w-full px-6 lg:px-40 mb-10">
+        <ul>
+          <li class="decoration-transparent">
+            <RouterLink to="/"><i class="fa fa-home mr-1"></i> Home</RouterLink>
+          </li>
+          <li class="decoration-transparent">
+            <RouterLink to="/posts"><i class="fa fa-book mr-1"></i> Blogs</RouterLink>
+          </li>
+          <li>{{ detail.title }}</li>
+        </ul>
+      </div>
+      <h1 class="mb-4 lg:text-4xl text-4xl tracking-tight leading-9 text-neutral font-extrabold dark:text-white">
+        {{ detail.title }}
+      </h1>
+      <p class="text-lg text-neutral dark:text-white">{{ detail.description }}</p>
+      <p class="text-md text-neutral dark:text-white mt-8">{{ detail.date }}</p>
+
+      <img class="mx-auto lg:w-2/5 w-4/5 my-7 rounded-md drop-shadow-sm" :src=detail.gambar :alt=detail.title>
+      <p class="text-neutral dark:text-white text-sm">posted by {{ detail.author }}</p>
+    </header>
+
+    <div class="w-full px-6 lg:px-40">
+      <p class="text-3xl mb-6 text-neutral dark:text-white font-bold">{{ detail.heading[0] }}</p>
+      <p class="text-md text-neutral dark:text-white mb-10">{{ detail.paragraf[0] }}</p>
+      <p class="text-md text-neutral dark:text-white">{{ detail.paragraf[1] }}</p>
+    </div>
+
+    <div class="w-full px-6 lg:px-40 mt-32">
+      <p class="text-3xl mb-6 text-neutral dark:text-white font-bold">{{ detail.heading[1] }}</p>
+      <p class="text-md text-neutral dark:text-white mb-10">{{ detail.paragraf[2] }}</p>
+      <p class="text-md text-neutral dark:text-white">{{ detail.paragraf[3] }}</p>
+      <p class="text-md text-neutral dark:text-white">{{ detail.paragraf[4] }}</p>
+    </div>
+
+
+  </div>
+
+  <Footer />
+</template>
+
+<script>
+import { blog } from '../data/Blog';
+import Footer from '../components/Footer.vue';
+
+export default {
+  props: [
+    "id_post"
+  ],
+  setup(props) {
+    const detail = blog["blog"].find(function (item) {
+      return item.id == props.id_post;
+    });
+    return {
+      detail
+    };
+  },
+
+}
+</script>
+
+<style>
+</style>
