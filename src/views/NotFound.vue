baf8d4f77883cfedfe78dafcafa05f4509049f1a
FthRobby <rbysalam10@gmail.com>
2022-09-28 17:09:33 +0700

    fixing bug

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="h-screen w-full flex flex-col justify-center items-center ">
-    <img src="../assets/404.svg" alt="">
+    <img src="../assets/404.svg" alt="not found">
     <div class="bg-green-500 text-white px-2 text-md rounded absolute">
       Page Not Found
     </div>
