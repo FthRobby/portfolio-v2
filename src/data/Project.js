89daf42d515c49f217d7158bdc8c6f0106b5d449
FthRobby <rbysalam10@gmail.com>
2022-10-07 23:00:00 +0700

    change data using export const

---
+++ src/data/Project.js
@@ -0,0 +1,40 @@
+export const project = {
+  project: [
+    {
+      id: 1,
+      name: "Ayo Masak",
+      description:
+        "Showing Indonesian Food and Drink Recipes. Data is fetched from Unofficial Masak Apa Hari Ini API and displayed in the website. The API is from Unofficial masapapahariini API.",
+      images: "../projects/ayo-masak/p1.png",
+      preview: "https://ayo-masak.vercel.app/",
+      repo: "https://github.com/",
+    },
+    {
+      id: "2",
+      name: "Notes App",
+      description:
+        "Notes App made using react js and tailwind css. This application was made as a final class submission at dicoding academy.",
+      images: "projects/notesapp/p1.png",
+      preview: "https://frobby-notesapp.vercel.app/",
+      repo: "https://github.com/",
+    },
+    {
+      id: "3",
+      name: "Mangan Apps",
+      description:
+        "Mangan App made using Webpack and CSS3. This application was made as a final class submission at dicoding academy.",
+      images: "projects/mangan/p1.png",
+      preview: "https://mangan-apps.vercel.app/",
+      repo: "https://github.com/",
+    },
+    {
+      id: "4",
+      name: "Hyundai Catalogue",
+      description:
+        "Catalogue of hyundai motors. Displaying the cars and their information. This project is for final submission of MSIB Dicoding.",
+      images: "projects/hyundai-catalogue/p1.png",
+      preview: "https://hyundai-catalogue.vercel.app/",
+      repo: "https://github.com/",
+    },
+  ],
+};
